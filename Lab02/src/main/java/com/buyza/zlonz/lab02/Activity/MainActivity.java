610b2026e6515107ba36a07a3ca265b6b59bf724
kanok <kanok@csc.co.th>
2016-03-02 00:03:09 +0700

    update lab02

---
+++ Lab02/src/main/java/com/buyza/zlonz/lab02/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.buyza.zlonz.lab02.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.buyza.zlonz.lab02.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
