bb20c323f5f8108e92b2a9a1901925213d9969e1
kanok <kanok@csc.co.th>
2016-03-01 19:39:02 +0700

    Initial commit

---
+++ Lab01/src/main/java/com/buyza/zlonz/projectlab/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.buyza.zlonz.projectlab.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.buyza.zlonz.projectlab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Lab01/src/main/java/com/buyza/zlonz/projectlab/MainApplication.java
@@ -0,0 +1,19 @@
+package com.buyza.zlonz.projectlab;
+
+import android.app.Application;
+
+/**
+ * Created by Zlonz on 1/3/2559.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Contextor.getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
